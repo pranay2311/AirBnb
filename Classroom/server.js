234a61b8831c7b22ab18aa6805de47b36762fb69
Pranay Shinde <PranayShinde486@gmail.com>
2025-07-06 19:12:15 +0530

    Flash

---
+++ Classroom/server.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const app = express();
+const path = require("path")
+const ejsMate = require("ejs-mate");
+const flash = require("connect-flash")
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+
+app.use(express.static(path.join(__dirname, "public")))
+
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
+
+
+const sessionOptions = {
+    secret: "Secret",
+    resave: false,
+    saveUninitialized: true
+}
+
+app.use(session(sessionOptions));
+app.use(flash())
+app.get("/test", (req, res) => {
+    res.send("Test")
+})
+
+app.get("/register", (req, res) => {
+    let { name = " " } = req.query;
+    req.session.name = name;
+    console.log(name);
+    console.log(req.session.name);
+    req.flash("success", "User Registered")
+    res.redirect("/hello")
+})
+
+app.get("/hello", (req, res) => {
+    res.render("page.ejs", { name: req.session.name, msg: req.flash("success") });
+    console.log(req.session)
+
+})
+
+
+
+
+
+app.get("/reqCount", (req, res) => {
+    if (req.session.count) {
+        req.session.count++
+    }
+    else {
+        req.session.count = 1
+    }
+    res.send(`You uSent ${req.session.count} request    `)
+})
+
+// app.get("/Cookie", (req, res) => {
+//     res.cookie("greet", "Hello");
+//     res.cookie("Name", "UserName");
+//     res.send("Cookie Sent")
+// })
+
+// app.get("/Greet", (req, res) => {
+//     let { Name="Pranay" }= req.cookies
+//     console.log(req.cookiesf)
+//     res.send(`Hii ${Name}`);
+// })
+
+
+app.use(cookieParser())
+app.listen(8080, () => {
+    console.log("Server Listening on 8080")
+})
